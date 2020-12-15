42290d3a8bb51fd6a446eb704383ef40e15eabd2
Tímea Makk <timea.makk@stud.uni-corvinus.hu>
2020-12-15 22:05:47 +0100

    11

--- Költségvetés_Elemző/Költségvetés_Elemző/Form1.Designer.cs
+++ Költségvetés_Elemző/Költségvetés_Elemző/Form1.Designer.cs
@@ -37,14 +37,14 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 81);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(796, 345);
+            this.dataGridView1.Size = new System.Drawing.Size(554, 345);
             this.dataGridView1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(835, 450);
+            this.ClientSize = new System.Drawing.Size(765, 450);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
